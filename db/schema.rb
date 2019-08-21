6f1424dd5d29cb19c41857b79889b89cb553c43f
Tan Theng Lee <thengleetan@gmail.com>
2019-08-21 12:40:51 +0800

    Add ability to follow other users
    Add follow_button helper
    Add FollowingRelationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_061113) do
+ActiveRecord::Schema.define(version: 2019_08_21_031541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "following_relationships", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "followed_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_user_id"], name: "index_following_relationships_on_followed_user_id"
+    t.index ["follower_id"], name: "index_following_relationships_on_follower_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id"
@@ -63,6 +72,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_061113) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "following_relationships", "users", column: "followed_user_id"
+  add_foreign_key "following_relationships", "users", column: "follower_id"
   add_foreign_key "likes", "shouts"
   add_foreign_key "likes", "users"
   add_foreign_key "shouts", "users"
